58128cacd9b3f16a3b200217097105ca54d6ba4b
Áõºƶ« <170776@nd.com>
2019-07-12 16:32:10 +0800

    init

---
+++ src/Component/react-element.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class ReactElement extends Component {
+  render() {
+    return (
+      <div className="exam">
+        <div style={{'margin-bottom': '20px'}}>demo
+            
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ReactElement;
